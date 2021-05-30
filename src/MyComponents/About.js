dc2204e7578a636a82eef19ac666abe0a74e7abe
mdobaid311 <81803774+mdobaid311@users.noreply.github.com>
2021-05-30 17:52:00 +0530

    initial build

---
+++ src/MyComponents/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      This is an about component
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis
+        tempore iste itaque optio rem? Porro nostrum animi blanditiis voluptas
+        non amet sapiente vitae debitis modi cum eos, repellat aut eum.
+      </p>
+    </div>
+  );
+};
